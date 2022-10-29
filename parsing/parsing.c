e96199b67c135ef1d39b1654b7bf3fd6d97fe658
Шамиль Атауллин <shamil@MacBook-Air.local>
2022-10-29 05:01:28 +0300

    state

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ntojamur <ntojamur@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:39:50 by ntojamur          #+#    #+#             */
-/*   Updated: 2022/10/29 04:28:09 by ntojamur         ###   ########.fr       */
+/*   Updated: 2022/10/29 04:59:34 by ntojamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,10 +66,40 @@ void	parse_repeats(t_state *cub)
 
 }
 
+void	init_arg(t_state *cub)
+{
+	int	i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (ft_strncmp(cub->info[i][0], "EA", 3) == 0)
+			cub->c_ea = cub->info[i][1];
+		else if (ft_strncmp(cub->info[i][0], "WE", 3) == 0)
+			cub->c_we = cub->info[i][1];
+		else if (ft_strncmp(cub->info[i][0], "NO", 3) == 0)
+			cub->c_no = cub->info[i][1];
+		else if (ft_strncmp(cub->info[i][0], "SO", 3) == 0)
+			cub->c_so = cub->info[i][1];
+		else if (ft_strncmp(cub->info[i][0], "F", 2) == 0)
+			cub->c_f = cub->info[i][1];
+		else if (ft_strncmp(cub->info[i][0], "C", 2) == 0)
+			cub->c_c = cub->info[i][1];
+		i++;
+	}
+}
+
 void	parsing(int argc, char **argv, t_state *cub)
 {
 	parse_conf(argc, argv);
 	cub->file = ft_get_file(argv[1]);
 	parse_strings(cub);
 	parse_repeats(cub);
+	init_arg(cub);
+	printf("%s\n",cub->c_c);
+	printf("%s\n",cub->c_f);
+	printf("%s\n",cub->c_ea);
+	printf("%s\n",cub->c_we);
+	printf("%s\n",cub->c_no);
+	printf("%s\n",cub->c_so);
 }
--- state/state.h
+++ state/state.h
@@ -6,7 +6,7 @@
 /*   By: ntojamur <ntojamur@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 16:44:00 by ntojamur          #+#    #+#             */
-/*   Updated: 2022/10/28 18:14:10 by ntojamur         ###   ########.fr       */
+/*   Updated: 2022/10/29 05:00:16 by ntojamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ typedef struct s_state
 	char		**file;
 	char		**info[6];
 	t_amount	data;
+	char		*c_ea;
+	char		*c_we;
+	char		*c_no;
+	char		*c_so;
+	char		*c_f;
+	char		*c_c;
 }	t_state;
 
 void	init_amount(t_amount *data);
